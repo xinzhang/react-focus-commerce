00c1874a8b07b74825147b4fa2615b47b1e36a4c
xin zhang <xin_zhang_2000@yahoo.com>
2017-02-25 15:53:20 +1100

    add account in top bar

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import CurrencyDropdown from './CurrencyDropdown';
 import LanguageDropdown from './LanguageDropdown';
@@ -17,12 +18,18 @@ class Topbar extends React.Component {
                       <div className="top-right pull-right">
                           <div id="top-links" className="nav pull-right">
                               <ul className="list-inline">
+                                  {this.props.account.status === '' &&
                                   <li className="dropdown"><a href="#" title="My Account" className="dropdown-toggle" data-toggle="dropdown"><i className="fa fa-user"></i> <span>My Account</span> <span className="caret"></span></a>
                                       <ul className="dropdown-menu dropdown-menu-right">
                                           <li><a href="/account/register">Register</a></li>
                                           <li><a href="/account/login">Login</a></li>
                                       </ul>
                                   </li>
+                                  }
+                                  {this.props.account.status === 'authenticated' &&
+                                    <li className="dropdown"><a href="#" title="My Account" className="dropdown-toggle" data-toggle="dropdown"><i className="fa fa-user"></i> <span>Welcome {this.props.account.firstname}</span></a>
+                                    </li>
+                                  }
                                   <li><a href="#" id="wishlist-total" title="Wish List (0)"><i className="fa fa-heart"></i> <span>Wish List</span><span> (0)</span></a></li>
                               </ul>
                           </div>
@@ -35,4 +42,10 @@ class Topbar extends React.Component {
   }
 }
 
-export default Topbar;
+
+function mapStateToProps(state, ownProps) {
+  return {
+    account: state.account
+  }
+}
+export default connect(mapStateToProps)( Topbar );
